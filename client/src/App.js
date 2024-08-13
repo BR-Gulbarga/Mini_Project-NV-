4016916a499be35402088156d4b95feee961a3c4
Naveenkumar <bracademy.gulbarga@gmail.com>
2024-08-13 15:50:06 +0530

    app.js is modified

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <h1 className="text-2xl font-bold mb-4">My Todo List</h1>
       <div className="mb-4">
         <input
           type="text"
